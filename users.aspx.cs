f453ad3edd1158892153fd2320c81d5a7f683b56
alaaban <alaa-banaamah@hotmail.com>
2016-04-20 20:32:21 +0300

    Insert city and users and restaurants

---
+++ users.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class users : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            SqlDataSource1.Insert();
+            Label1.Text = "It has been added successfully!";
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+
+        }
+        catch(Exception ex)
+        {
+            Label1.Text = ex.ToString();
+        }
+    }
+}
